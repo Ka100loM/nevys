176a0b026a9b80ed6bb5c90e59cdbca32b62bf94
Costea <hook41@gmail.com>
2013-05-09 00:56:39 +0300

    intialised rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nevys::Application.config.secret_token = '77812b2f08390037b109ae866f48991bf7ec50692fcd13239a82c3843990b1a331fe5833829be9136e93a2263fb68c2b701a90a2076ad73f04c4a9d3a173a867'
